f754de4a3077f95a93f4e62799e5e3746a09aeca
Paweł Feruś <panther.mindc@gmail.com>
2018-12-10 14:37:53 +0100

    put test

--- test/test.php
+++ test/test.php
@@ -6,7 +6,13 @@ $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
 $dotenv->load();
 
 $client = new \AGSystems\REST\Toggl\Client(
-    getenv('TOGGL_TOKEN')
+    getenv('API_TOKEN')
 );
 
 var_export($client->me->get());
+
+var_export($client->time_entries(1047990241)->put([
+    'time_entry' => [
+        'description' => '(e) Motonet :: wyciągnięcie listy zestawów ' . uniqid(),
+    ],
+]));
